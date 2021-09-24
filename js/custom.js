9b3eeb69588d97b3cace571e5fce6acd5167de27
Shaun Porter <shaunoman840@gmail.com>
2021-09-24 22:20:54 +0100

    Carasol

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ function initfade() {
     let headerElem = document.getElementsByClassName("header");
     headerElem = headerElem[0];
     console.log(headerElem);
-    for(let i = 0; headerElem.children.length > i; i++) {
+    for (let i = 0; headerElem.children.length > i; i++) {
         setTimeout(() => {
             headerElem.children[i].style.opacity = "1";
             headerElem.children[i].classList.add("animate__fadeInUp")
@@ -11,12 +11,12 @@ function initfade() {
 
     let videoElems = document.getElementById("videoShowcase")
     let videoAnims = ["animate__fadeInLeft", "animate__fadeInUp", "animate__fadeInRight"];
-    for(let i = 0; videoElems.children.length > i; i++) {
+    for (let i = 0; videoElems.children.length > i; i++) {
         setTimeout(() => {
             videoElems.children[i].style.opacity = "1";
             videoElems.children[i].classList.add(videoAnims[i]);
         }, i * 300, videoElems)
-        
+
 
     }
 }
@@ -29,3 +29,13 @@ setTimeout(initfade, 200);
 tippy(".tippyHover", {
     content: "Tooltip"
 })
+
+
+$(document).ready(function () {
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+});
